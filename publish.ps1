441c1faede21c429f36fef9eb740393347b4723f
Peter Ullrich <dotup.software@gmail.com>
2021-02-16 13:21:26 +0100

    api key removed :)

--- publish.ps1
+++ publish.ps1
@@ -2,5 +2,8 @@ Import-Module DotupPsEssentials;
 Import-Module DotupPsModuleGenerator;
 
 $path = Get-Location;
-$PowershellApiKey = "oy2brhym6syjwgjiwnoqhnh56zx52pju7b27ravqwewwwy";
-Publish-PsModule -Path $path -APIKey $PowershellApiKey -IncrementVersion;
+
+$PowershellApiKey = "jo";
+# Publish-PsModule -Path $path -APIKey $PowershellApiKey -IncrementVersion;
+
+Publish-PsModule -Path $path -IncrementVersion;
